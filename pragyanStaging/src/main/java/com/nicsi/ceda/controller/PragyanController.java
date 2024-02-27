a454bb80713fca1ec9c500d3a5e0985f84986459
ashunicsi <ashunicsi@gmail.com>
2024-02-27 11:14:16 +0530

    First Commit

--- pragyanStaging/src/main/java/com/nicsi/ceda/controller/PragyanController.java
+++ pragyanStaging/src/main/java/com/nicsi/ceda/controller/PragyanController.java
@@ -6,16 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PragyanController 
 {
-	@GetMapping
-	public String pragyan()
-	{
-		System.out.println("Test");
-		return "";
-	}
-	@GetMapping
-	public String pragyan1()
-	{
-		System.out.println("Test");
-		return ""; 
-	}  
+	
 }
